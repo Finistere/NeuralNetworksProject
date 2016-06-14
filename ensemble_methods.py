a178e83685a3f62542eabadfee3aea32b7f6f7c3
Alexander Goscinski <a.goscinski@mail.tu-berlin.de>
2016-06-14 13:35:23 +0200

    changed ensemble rank combination to weight combinaton

--- ensemble_methods.py
+++ ensemble_methods.py
@@ -18,10 +18,18 @@ class Stacking(FeatureRanking):
         features_rankings = np.array(features_rankings)
         return self.rank_weights(self.combine(features_rankings))
 
+    def weight(self, data, classes):
+        features_weights = []
+        for feature_ranking_method in self.feature_ranking_methods:
+            features_weights.append(feature_ranking_method.weight(data, classes))
+        features_weights = np.array(features_weights)
+        return self.combine(features_weights)
+
     def combine(self, features_rankings):
         if self.combination == "mean":
             return np.power(features_rankings, self.p).mean(axis=0)
         if self.combination == "hmean":
-            return scipy.stats.hmean(np.power(features_rankings, self.p), axis=0)
+            regularization_parameter = 1e-15
+            return scipy.stats.hmean(np.power(features_rankings + regularization_parameter, self.p), axis=0)
         raise Exception("Unknown combination")
 
