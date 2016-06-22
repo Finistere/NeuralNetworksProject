b918f0105f6f350180cbd53c35e5278c780793f4
Alexander Goscinski <a.goscinski@mail.tu-berlin.de>
2016-06-22 15:52:14 +0200

    Added directory class; changed analysis results pkl output to csv

--- test_weights.py
+++ test_weights.py
@@ -1,9 +1,13 @@
 from analyse_weights import AnalyseBenchmarkResults
-from feature_selector import SymmetricalUncertainty, Relief, SVM_RFE
+from feature_selector import SymmetricalUncertainty, Relief, SVM_RFE, LassoFeatureSelector
 
 
 feature_selectors = [
-    SymmetricalUncertainty()]
+    SymmetricalUncertainty(),
+    Relief(),
+    SVM_RFE(),
+    LassoFeatureSelector()
+]
 
 analysis = AnalyseBenchmarkResults(feature_selectors)
-analysis.run("arcene")
+analysis.run("colon")
